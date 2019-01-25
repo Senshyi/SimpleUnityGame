5584819a70e11a2dbf7260537d48e8b68e3a206e
jan machacek <janmachacek97@gmail.com>
2019-01-25 22:36:25 +0100

    add player movement functionality

---
+++ SimpleGame/Assets/PlayerMovement.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public Rigidbody rb;
+    public float forwardForce = 1000f;
+    public float sidewaysForce = 500f;
+
+    void Update()
+    {
+
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if(Input.GetKey("d"))
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
+        }
+    }
+}
