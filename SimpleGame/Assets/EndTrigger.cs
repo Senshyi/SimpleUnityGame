e266837ab694f8675daa14b2468a28d2c9b03c74
jan machacek <janmachacek97@gmail.com>
2019-01-27 05:14:06 +0100

    add levelComplete function

---
+++ SimpleGame/Assets/EndTrigger.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class EndTrigger : MonoBehaviour
+{
+
+    public GameManager gameManager; 
+
+    private void OnTriggerEnter()
+    {
+        gameManager.LevelComplete();
+    }
+}
---
+++ SimpleGame/Assets/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+
+    private bool gameHasEnded = false;
+    public float resetTimer = 2f;
+
+    public void LevelComplete()
+    {
+        Debug.Log("Level Complete!");
+    }
+
+    public void GameOver()
+    {
+        if (gameHasEnded == false)
+        {
+            gameHasEnded = true;
+            Debug.Log("Game Over!");
+            Invoke("Reset", resetTimer);
+        }
+    }
+
+    private void Reset()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+}
